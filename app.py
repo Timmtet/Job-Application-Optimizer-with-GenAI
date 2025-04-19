7a57831a25a3b3ef725063a986fe082cce13f1a4
Timileyin Akintilo <96595056+Timmtet@users.noreply.github.com>
2025-04-19 09:16:07 +0100

    Update  to use Streamlit secrets for API key

--- app.py
+++ app.py
@@ -9,9 +9,12 @@ import os
 
 
 # load the environment variables from the .env file
-load_dotenv()
+# load_dotenv()
 
-genai.configure(api_key= os.getenv("GOOGLE_API_KEY"))
+# genai.configure(api_key= os.getenv("GOOGLE_API_KEY"))
+
+# using streamlit secret instead of the .env file
+api_key = st.secrets["GOOGLE_API_KEY"]
 
 # -----------------------
 # CONFIGURE YOUR GEMINI API
